bbd7c95cf6c8fe1a90965f759b4f59c0efcdcd12
Prateek Singh <Prateeksingh1590@gmail.com>
2016-11-11 16:21:04 +0530

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,15 @@
+name = "Prateek Singh"
+age = 35
+height = 76
+weight = 75
+eyes = "brown"
+teeth = "white"
+hair = "black"
+
+print("Let's talk about %s" % name)
+print("%s is %d inch tall" % (name, height))
+print("%s is %d Kg in weight" % (name, weight))
+print("he's got %s eyes and %s hairs" %(eyes, hair))
+print("and teeth are of %s color" % teeth)
+
+print("If I add %d, %d and %d , we'll get %d" %(height, weight, age, age+height+weight))
